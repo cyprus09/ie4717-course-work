2560fc665ebe294201fd89fb9fbcfabfb4c2bad4
mayankpallai <mayankpallai@gmail.com>
2024-09-06 20:23:03 +0800

    add css and update html for case 3

--- Week5/case-study-3/src/FormValidationExample.jsx
+++ Week5/case-study-3/src/FormValidationExample.jsx
@@ -32,11 +32,14 @@ const FormValidationExample = () => {
     } else if (!/\S+@\+\.\S|/.test(formData.email)) {
       validationErrors.email = "email is not valid";
     }
-
     if (!formData.password.trim()) {
       validationErrors.password = "password is required";
     } else if (formData.password.length < 6) {
       validationErrors.password = "password should be atleast 6 characters";
+    } else if (!/[0-9]/.test(formData.password)) {
+      validationErrors.password = "password should have atleast one number";
+    } else if (!/[!@#$%^&*(),.?":{}|<>]/.test(formData.password)) {
+      validationErrors.password = "password should have atleast one special character";
     }
 
     if (formData.password !== formData.confirmPassword) {
@@ -52,56 +55,64 @@ const FormValidationExample = () => {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username:</label>
-          <input
-            type="text"
-            name="userName"
-            placeholder="Enter your username"
-            onChange={handleChange}
-            required
-            autoComplete="off"
-          />
-          {errors.username && <span>{errors.username}</span>}
+      <div className="form-holder">
+        <div className="form-content">
+          <div className="form-items">
+            <h3>Register Now</h3>
+            <form onSubmit={handleSubmit}>
+              <input
+                className="form-control"
+                onChange={handleChange}
+                type="text"
+                name="username"
+                placeholder="Enter your Full Name"
+                required
+              />
+              {errors.username && <span>{errors.username}</span>}
+
+              <input
+                className="form-control"
+                onChange={handleChange}
+                type="email"
+                name="email"
+                placeholder="Enter your Email-ID"
+                required
+              />
+              {errors.email && <span>{errors.email}</span>}
+
+              <input
+                className="form-control"
+                onChange={handleChange}
+                type="password"
+                name="password"
+                placeholder="Enter your Password"
+                required
+              />
+              {errors.password && <span>{errors.password}</span>}
+
+              <input
+                className="form-control"
+                onChange={handleChange}
+                type="password"
+                name="confirmPassword"
+                placeholder="Re-enter your Password"
+                required
+              />
+              {errors.confirmPassword && <span>{errors.confirmPassword}</span>}
+
+              <div className="form-check">
+                <br />
+                <input className="form-check-input" type="checkbox" required />I confirm that all data entered is
+                correct
+              </div>
+
+              <button id="submit" type="submit" className="btn btn-primary">
+                Register
+              </button>
+            </form>
+          </div>
         </div>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            name="emailId"
-            placeholder="Enter your email-id"
-            onChange={handleChange}
-            required
-            autoComplete="off"
-          />
-          {errors.email && <span>{errors.email}</span>}
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            name="passWord"
-            placeholder="Enter your password"
-            onChange={handleChange}
-            required
-            autoComplete="off"
-          />
-          {errors.password && <span>{errors.password}</span>}
-        </div>
-        <div>
-          <label>Confirm Password:</label>
-          <input
-            type="password"
-            name="confirmPassword"
-            placeholder="Re-enter your password"
-            onChange={handleChange}
-            required
-            autoComplete="off"
-          />
-          {errors.confirmPassword && <span>{errors.confirmPassword}</span>}
-        </div>
-      </form>
+      </div>
     </>
   );
 };
