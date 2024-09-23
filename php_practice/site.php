e27a5c2623bae30bb8034ffcd52b4ac1db395196
mayankpallai <mayankpallai@gmail.com>
2024-09-23 21:14:19 +0800

    php practice

---
+++ php_practice/site.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  $name = 'George';
+  $age = 70;
+  echo 'there was once a man named ' . $name . '<br>';
+  echo ('he was ' . $age . ' years old <br>');
+  echo ('he liked being ' . $name . '<br>');
+  echo ('but didnt like being ' . $age . '<br>');
+  ?>
+</body>
+
+</html>
