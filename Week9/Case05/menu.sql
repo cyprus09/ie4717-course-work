fa8e3669b82234375282cf86c2b108340e9e1af6
mayankpallai <mayankpallai@gmail.com>
2024-10-19 03:22:18 +0800

    add sql for menu prices and sales report

--- Week9/Case05/menu.sql
+++ Week9/Case05/menu.sql
@@ -1,12 +1,66 @@
--- create database coffee_prices use coffee_prices;
+-- create the coffee prices db
+create database coffee_prices use coffee_prices;
 
--- create table
---   prices (coffee_items varchar(50), price_items decimal(5, 2));
+-- create the table to track prices
+create table
+  prices (coffee_type varchar(30), price decimal(5, 2));
 
+-- create the table to track sales and revenue
+create table
+  sales (
+    coffee_type varchar(30),
+    product_name varchar(30),
+    category varchar(30),
+    quantity int,
+    revenue decimal(10, 2)
+  );
 
--- insert into prices values
--- ('justjava', 2.00)
--- ('cafeaulaitsingle', 2.00)
--- ('cafeaulaitdouble', 2.00)
--- ('justjava', 2.00)
--- ('justjava', 2.00)
+-- insert init values into prices tables
+insert into
+  prices
+values
+  ('justJava', 2.00),
+  ('cafeAuLaitSingle', 2.00),
+  ('cafeAuLaitDouble', 3.00),
+  ('icedCappucinoSingle', 4.75),
+  ('icedCappucinoDouble', 5.75);
+
+-- insert init values into sales tables
+insert into
+  sales
+values
+  ('justJava', 'Just Java', NULL, 0, 0.00),
+  (
+    'cafeAuLaitSingle',
+    'Cafe Au Lait',
+    'Single',
+    0,
+    0.00
+  ),
+  (
+    'cafeAuLaitDouble',
+    'Cafe Au Lait',
+    'Double',
+    0,
+    0.00
+  ),
+  (
+    'icedCappucinoSingle',
+    'Iced Cappucino',
+    'Single',
+    0,
+    0.00
+  ),
+  (
+    'icedCappucinoDouble',
+    'Iced Cappucino',
+    'Double',
+    0,
+    0.00
+  );
+
+-- resetting variables within the sales table if called
+update sales
+set
+  quantity = 0,
+  revenue = 0.00;
